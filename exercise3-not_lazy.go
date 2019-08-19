929556e2dcc1acf51468d4317bbdd3f6562ae517
Margaret G <Margaret.Gorguissian@tufts.edu>
2019-08-18 19:11:57 -0700

    Added attribution

--- exercise3-not_lazy.go
+++ exercise3-not_lazy.go
@@ -8,7 +8,9 @@ import (
 	"sort"
 )
 
-var mostCommonCharacters = map[string]float64 {
+// characterFrequency map from ricpacca/cryptopals
+
+var characterFrequency = map[string]float64 {
     "a": 0.0651738, "b": 0.0124248, "c": 0.0217339, "d": 0.0349835, "e": 0.1041442, "f": 0.0197881, "g": 0.0158610,
     "h": 0.0492888, "i": 0.0558094, "j": 0.0009033, "k": 0.0050529, "l": 0.0331490, "m": 0.0202124, "n": 0.0564513,
     "o": 0.0596302, "p": 0.0137645, "q": 0.0008606, "r": 0.0497563, "s": 0.0515760, "t": 0.0729357, "u": 0.0225134,
@@ -23,7 +25,7 @@ func getEnglishScore(s string) float64{
 	score := float64(0)
 	if isAlpha.MatchString(s){
 		for i := 0; i < length; i++{
-			score = score + mostCommonCharacters[string(s[i])]
+			score = score + characterFrequency[string(s[i])]
 		}
 	}
 	return score
